cf15d2a4f350302a88df39a1b2aea9a73f8dd882
Sebziel <sebzie1996@gmail.com>
2022-08-13 20:07:09 +0200

    adding app version and platform version as params to template

--- vars/InitPipeline.groovy
+++ vars/InitPipeline.groovy
@@ -4,7 +4,11 @@ def call() {
     stages{
         stage('Playbook-sleep-test'){
             steps{
-                sh "ansible-playbook -i /var/lib/jenkins/Ansible/inventory /var/lib/jenkins/Ansible/test-playbook.yaml --tags template -e build_number=${BUILD_NUMBER}" 
+                    script {
+                        pom_platform_version = params.PLATFORM_VERSION
+                        pom_appl_version = params.EXTENSIONS_VERSION
+                    }
+                sh "ansible-playbook -i /var/lib/jenkins/Ansible/inventory /var/lib/jenkins/Ansible/test-playbook.yaml --tags template -e build_number=${BUILD_NUMBER} -e pom_platform_version=$pom_platform_version -e pom_appl_version=${params.EXTENSIONS_VERSION}"
             }
         }
         stage('pingtest'){
