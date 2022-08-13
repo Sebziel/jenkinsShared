5290bcd6ddfec5663972dc6226120f82611d8ddf
Sebziel <sebzie1996@gmail.com>
2022-08-13 18:33:40 +0200

    fixing syntax for build number variable

--- vars/InitPipeline.groovy
+++ vars/InitPipeline.groovy
@@ -4,7 +4,7 @@ def call() {
     stages{
         stage('Playbook-sleep-test'){
             steps{
-                sh "ansible-playbook -i /var/lib/jenkins/Ansible/inventory /var/lib/jenkins/Ansible/test-playbook.yaml --tags template -e ${BUILD_NUMBER}" 
+                sh "ansible-playbook -i /var/lib/jenkins/Ansible/inventory /var/lib/jenkins/Ansible/test-playbook.yaml --tags template -e build_number=${BUILD_NUMBER}" 
             }
         }
         stage('pingtest'){
