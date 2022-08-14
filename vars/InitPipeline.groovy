bda871bc584d00177e25090fee5adc66f14e3036
Sebziel <sebzie1996@gmail.com>
2022-08-14 19:58:10 +0200

    nomsg

--- vars/InitPipeline.groovy
+++ vars/InitPipeline.groovy
@@ -1,8 +1,5 @@
-def call(body) {
+def call() {
     def pipelineParams = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = pipelineParams
-    body()
     pipeline{
     agent any
     stages{
