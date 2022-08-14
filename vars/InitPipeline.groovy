63f3768078952a269a499c63a2c8d0dad67e6099
Sebziel <sebzie1996@gmail.com>
2022-08-14 19:34:19 +0200

    nomessage

--- vars/InitPipeline.groovy
+++ vars/InitPipeline.groovy
@@ -5,13 +5,6 @@ def call() {
             booleanParam 'BoolFromGroovy'
     }
     stages{
-        stage('Params'){
-            steps{
-                parameters {
-                     booleanParam 'BoolFromGroovy2'
-                }
-            }
-        }
         stage('Playbook-sleep-test'){
             steps{
                 sh "ansible-playbook -i /var/lib/jenkins/Ansible/inventory /var/lib/jenkins/Ansible/test-playbook.yaml --tags template -e build_number=${BUILD_NUMBER} -e pom_platform_version=${params.PLATFORM_VERSION} -e pom_appl_version=${params.EXTENSIONS_VERSION}"
